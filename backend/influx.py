f86d2bccffb9208748057e47ad430402bfb794f5
Finn Welzmüller <finnwelzmueller97@gmail.com>
2025-08-15 22:46:03 +0200

    added correct influxdb config parameters from .env file

--- backend/influx.py
+++ backend/influx.py
@@ -4,24 +4,23 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+
 env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 # Configuration via environment variables or fallback defaults
-INFLUXDB_URL = os.getenv("INFLUXDB_URL", "http://localhost:8086")
-INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN", "your-token")
-INFLUXDB_ORG = os.getenv("INFLUXDB_ORG", "your-org")
-INFLUXDB_BUCKET = os.getenv("INFLUXDB_BUCKET", "sshame")
+INFLUXDB_URL = os.getenv("INFLUXDB_URL", None)
+INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN", None)
 
 # Initialize the InfluxDB client
 client = InfluxDBClient(
     url=INFLUXDB_URL,
     token=INFLUXDB_TOKEN,
-    org=INFLUXDB_ORG,
+    org="sshame",
 )
 
 write_api = client.write_api()
 
-def write_ssh_event(ip, user, timestamp_str, port=None):
+def write_ssh_event(ip, user, timestamp_str, port=None, country_long=None, lat=None, lon=None):
     """
     Writes a single SSH failure event to InfluxDB.
 
@@ -45,12 +44,18 @@ def write_ssh_event(ip, user, timestamp_str, port=None):
         )
 
         # Optionally include port tag
-        if port is not None:
+        if port:
             point.tag("port", str(port))
+        if country_long:
+            point.tag("country", country_long)
+        if lat is not None:
+            point.field("lat", float(lat))
+        if lon is not None:
+            point.field("lon", float(lon))
 
         # Write to InfluxDB
-        write_api.write(bucket=INFLUXDB_BUCKET, org=INFLUXDB_ORG, record=point)
-        print(f"Wrote to influxDB: ip: {ip}, user: {user}, timestamp: {timestamp_str}, port: {port}.")
+        write_api.write(bucket="sshame", org="sshame", record=point)
+        print(f"Wrote to influxDB: ip: {ip}, user: {user}, timestamp: {timestamp_str}, port: {port}, country: {country_long}, lat: {lat}, lon: {lon}.")
 
     except Exception as e:
         print(f"Failed to write event to InfluxDB: {e}")
