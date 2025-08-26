d0cdda9bf2f27525efd7fbcead73100221670cfd
Finn Welzmüller <finnwelzmueller97@gmail.com>
2025-08-26 22:08:34 +0200

    moved to backend folder

--- backend/mysql/init/init.sql
+++ backend/mysql/init/init.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE IF NOT EXISTS sshame
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE sshame;
+
 CREATE TABLE IF NOT EXISTS ip_locations (
     ip_from BIGINT,
     ip_to BIGINT,
@@ -14,3 +18,15 @@ INTO TABLE ip_locations
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 (ip_from, ip_to, country_short, country_long, region, city, lat, lon);
+
+
+CREATE TABLE IF NOT EXISTS ssh_events (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    ip VARCHAR(45),
+    username VARCHAR(100),
+    ts DATETIME,
+    port INT,
+    country VARCHAR(100),
+    lat DOUBLE,
+    lon DOUBLE
+);
