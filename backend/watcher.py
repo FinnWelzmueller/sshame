cd95d71af639fc7c6d174f76f2b4b299ffced750
Finn <finnwelzmueller97@gmail.com>
2025-10-01 08:27:37 +0200

    added comments in watcher.py

--- backend/watcher.py
+++ backend/watcher.py
@@ -26,19 +26,33 @@ STATE_FILE = "/state/state.json"
 
 
 
-def load_state():
+def load_state() -> int:
+    """
+    loads the last processed row from state file
+    if state file does not exist, return 0 (meaning scan whole log)
+    :return: last processed row number
+    """
     if not os.path.exists(STATE_FILE):
         return 0
     with open(STATE_FILE, "r") as f:
         return json.load(f)["last_row"]
     
 
-def save_state(last_row: int):
+def save_state(last_row: int) -> None:
+    """
+    saves the last processed row to state file.
+    :param last_row: last processed row number to be saved
+    """
     os.makedirs(os.path.dirname(STATE_FILE), exist_ok=True)
     with open(STATE_FILE, "w") as f:
         json.dump({"last_row": last_row}, f)
 
 def read_lines(path:str) -> list[str]:
+    """
+    reads all lines from a file, saved at path, as a list of strings.
+    :param path: path to the file to be read
+    :return: list of lines read from the file
+    """
     try:
         with open(path, "r") as f:
             lines = f.readlines()
@@ -48,6 +62,11 @@ def read_lines(path:str) -> list[str]:
         return []
 
 def read_new_lines() -> list[str]:
+    """
+    loads new lines from the log file since last processed row. 
+    Handles log rotation.
+    :return: list of new lines read from the log file
+    """
     last_row = load_state()
     lines = read_lines(LOG_PATH) or []
     curr_len = len(lines)
@@ -65,6 +84,9 @@ def read_new_lines() -> list[str]:
     return tail + lines
 
 def run_watcher():
+    """
+    main loop of the watcher. Reads new lines every x seconds (exact number to be found in env file)
+    """
     logging.info("Starting sshame watcher...")
     while True:
         new_lines = read_new_lines()
